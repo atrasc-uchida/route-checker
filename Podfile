0bb0e09bbcdebe111f060b98651d66a3392c2865
atrasc-uchida <uchida@atrasc.co.jp>
2020-09-17 09:39:20 +0900

    first commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '12.4'
+
+target 'route-checker' do
+  use_frameworks!
+
+  pod 'TinyConstraints'
+  pod 'IBAnimatable'
+  pod 'Alertift'
+  pod 'Toast-Swift'
+  pod 'FontAwesome.swift'
+  pod 'Alamofire'
+end
---
+++ route-checker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  route-checker
+//
+//  Created by Harunori Uchida on 2020/09/17.
+//  Copyright © 2020 atrasc.co.jp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
